3349bac6646fbf09529af1931853e7946c38a41f
Thanh Duong <thanhduonghd114@gmail.com>
2024-08-30 16:23:31 +0300

    base code

--- lib/ci-cd-aws-pipeline-stack.ts
+++ lib/ci-cd-aws-pipeline-stack.ts
@@ -1,16 +1,17 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 
 export class CiCdAwsPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CiCdAwsPipelineQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'TestPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('alexha11/aws-ci-cd-pipeline', 'main'),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth']
+      })
+    });
   }
 }
